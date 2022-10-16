c07c1076b55c37552efd8717140dd1aacfc0cedd
Maxim Sorokin <maximka773@mail.ru>
2022-10-16 11:33:22 +0300

    3 commit

--- Program.cs
+++ Program.cs
@@ -1,42 +1,79 @@
-﻿int GetRandomNumber(int minValue, int maxValue)
+﻿// int GetRandomNumber(int minValue, int maxValue)
+// {
+//     Random random = new Random();
+//     int randomNumber = random.Next(minValue, maxValue);
+//     return randomNumber;
+// }
+
+// int FindMaxValue(int number)
+// {
+//     int max = number % 10;
+//     number = number / 10;
+
+//     while (number > 0)
+//     {
+//         int lastNumber = number % 10;
+//         if (lastNumber > max)
+//         {
+//             max = lastNumber;
+//         }
+//         number = number / 10;
+//     }
+//     return max;
+// }
+
+// int DeleteSecondDigit(int number)
+// {
+//     int lastNumber = number % 10;
+//     number = number / 100;
+
+//     int newNumber = number * 10 + lastNumber;
+
+//     return newNumber;
+// }
+
+// bool CheckMultiplicity(int firstNumber)
+// {
+//     int remains1 = firstNumber % 7;
+//     int remains2 = firstNumber % 23;
+
+//     return remains1 == 0 & remains2 == 0;
+// }
+
+bool CheckSquare(int firstNumber)
 {
-    Random random = new Random();
-    int randomNumber = random.Next(minValue, maxValue);
-    return randomNumber;
+    int square = firstNumber % 7;
+    int remains2 = firstNumber % 23;
+
+    return remains1 == 0 & remains2 == 0;
 }
 
-int FindMaxValue(int number)
-{
-    int max = number % 10;
-    number = number / 10;
 
-    while (number > 0)
-    {
-        int lastNumber = number % 10;
-        if (lastNumber > max)
-        {
-            max = lastNumber;
-        }
-        number = number / 10;
-    }
-    return max;
+// Console.WriteLine("Введите два числа");
+// int A = Convert.ToInt32(Console.ReadLine());
+// int B = Convert.ToInt32(Console.ReadLine());
+
+// int remainsNumber = CheckMultiplicity(A, B);
+
+Console.WriteLine("Введите число");
+int A = Convert.ToInt32(Console.ReadLine());
+
+bool result = CheckMultiplicity(A);
+
+if (result)
+{
+    Console.WriteLine("Кратно");
+}
+else
+{
+    Console.WriteLine($"Не кратно");
 }
 
-int DeleteSecondDigit(int number)
-{
-    int lastNumber = number % 10;
-    number = number / 100;
-    
-    int newNumber = number * 10 + lastNumber;
+// int randomNumber = GetRandomNumber(100, 1000);
+// Console.WriteLine(randomNumber);
 
-    return newNumber;
-}
-
-int randomNumber = GetRandomNumber(100, 1000);
-Console.WriteLine(randomNumber);
-
-int number2 = DeleteSecondDigit(randomNumber);
-Console.WriteLine(number2);
+// int number2 = DeleteSecondDigit(randomNumber);
+// Console.WriteLine(number2);
 
 
 // int array = new int[5];
