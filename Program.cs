04c34c75a2e794a7c372c46bf5df4d8113cfd8f3
Maxim Sorokin <maximka773@mail.ru>
2022-10-16 14:22:45 +0300

    расписал 11, 12, 14, 16 задачи

--- Program.cs
+++ Program.cs
@@ -1,44 +1,51 @@
-﻿// int GetRandomNumber(int minValue, int maxValue)
-// {
-//     Random random = new Random();
-//     int randomNumber = random.Next(minValue, maxValue);
-//     return randomNumber;
-// }
+﻿int GetRandomNumber(int minValue, int maxValue)
+{
+    Random random = new Random();
+    int randomNumber = random.Next(minValue, maxValue);
+    return randomNumber;
+}
 
-// int FindMaxValue(int number)
-// {
-//     int max = number % 10;
-//     number = number / 10;
+int FindMaxValue(int number)
+{
+    int max = number % 10;
+    number = number / 10;
 
-//     while (number > 0)
-//     {
-//         int lastNumber = number % 10;
-//         if (lastNumber > max)
-//         {
-//             max = lastNumber;
-//         }
-//         number = number / 10;
-//     }
-//     return max;
-// }
+    while (number > 0)
+    {
+        int lastNumber = number % 10;
+        if (lastNumber > max)
+        {
+            max = lastNumber;
+        }
+        number = number / 10;
+    }
+    return max;
+}
 
-// int DeleteSecondDigit(int number)
-// {
-//     int lastNumber = number % 10;
-//     number = number / 100;
+int DeleteSecondDigit(int number)
+{
+    int lastNumber = number % 10;
+    number = number / 100;
 
-//     int newNumber = number * 10 + lastNumber;
+    int newNumber = number * 10 + lastNumber;
 
-//     return newNumber;
-// }
+    return newNumber;
+}
 
-// bool CheckMultiplicity(int firstNumber)
-// {
-//     int remains1 = firstNumber % 7;
-//     int remains2 = firstNumber % 23;
+int CheckMultiplicity(int firstNumber, int secondNumber)
+{
+    int remains = firstNumber % secondNumber;
 
-//     return remains1 == 0 & remains2 == 0;
-// }
+    return remains;
+}
+
+bool CheckMultiplicity723(int firstNumber)
+{
+    int remains1 = firstNumber % 7;
+    int remains2 = firstNumber % 23;
+
+    return remains1 == 0 & remains2 == 0;
+}
 
 bool CheckSquare(int firstNumber, int secondNumber)
 {
@@ -48,34 +55,75 @@ bool CheckSquare(int firstNumber, int secondNumber)
     return square1 == secondNumber | square2 == firstNumber;
 }
 
-Console.WriteLine("Введите два числа");
-int A = Convert.ToInt32(Console.ReadLine());
-int B = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+Console.WriteLine("Введите номер задачи: 11, 12, 14 или 16");
+int z = Convert.ToInt32(Console.ReadLine());
 
-bool result = CheckSquare(A, B);
-
-if (result)
+// Задача 11
+if (z == 11)
 {
-    Console.WriteLine("Да");
+    int randomNumber = GetRandomNumber(100, 1000);
+    Console.WriteLine(randomNumber);
+
+    int number2 = DeleteSecondDigit(randomNumber);
+    Console.WriteLine(number2);
 }
-else
+
+// Задача 12
+if (z == 12)
 {
-    Console.WriteLine($"Нет");
+    Console.WriteLine("Введите два числа");
+    int A12 = Convert.ToInt32(Console.ReadLine());
+    int B12 = Convert.ToInt32(Console.ReadLine());
+
+    int remainsNumber12 = CheckMultiplicity(A12, B12);
+
+    if (remainsNumber12 == 0)
+    {
+        Console.WriteLine("Кратно");
+    }
+    else
+    {
+        Console.WriteLine($"Не кратно, остаток {remainsNumber12}");
+    }
 }
 
-// int remainsNumber = CheckMultiplicity(A, B);
+// Задача 14
+if (z == 14)
+{
+    Console.WriteLine("Введите число");
+    int A14 = Convert.ToInt32(Console.ReadLine());
 
-// Console.WriteLine("Введите число");
-// int A = Convert.ToInt32(Console.ReadLine());
+    bool result14 = CheckMultiplicity723(A14);
 
-// bool result = CheckMultiplicity(A);
+    if (result14)
+    {
+        Console.WriteLine("Число кратно одновременно 7 и 23");
+    }
+    else
+    {
+        Console.WriteLine($"Число НЕ кратно одновременно 7 и 23");
+    }
+}
 
-// int randomNumber = GetRandomNumber(100, 1000);
-// Console.WriteLine(randomNumber);
+// Задача 16
+if (z == 16)
+{
+    Console.WriteLine("Введите два числа");
+    int A = Convert.ToInt32(Console.ReadLine());
+    int B = Convert.ToInt32(Console.ReadLine());
 
-// int number2 = DeleteSecondDigit(randomNumber);
-// Console.WriteLine(number2);
+    bool result = CheckSquare(A, B);
 
+    if (result)
+    {
+        Console.WriteLine("Одно число является квадратом другого числа");
+    }
+    else
+    {
+        Console.WriteLine($"Одно число НЕ является квадратом другого числа");
+    }
+}
 
 // int array = new int[5];
 
