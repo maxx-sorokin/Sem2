3db85d46e97408de55d68c46d7a9fb75e7700057
Maxim Sorokin <maximka773@mail.ru>
2022-10-16 10:34:39 +0300

    2 commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int GetRandomNumber(int minValue, int maxValue)
+{
+    Random random = new Random();
+    int randomNumber = random.Next(minValue, maxValue);
+    return randomNumber;
+}
+
+int FindMaxValue(int number)
+{
+    int max = number % 10;
+    number = number / 10;
+
+    while (number > 0)
+    {
+        int lastNumber = number % 10;
+        if (lastNumber > max)
+        {
+            max = lastNumber;
+        }
+        number = number / 10;
+    }
+    return max;
+}
+
+int randomNumber = GetRandomNumber(10, 100);
+Console.WriteLine(randomNumber);
+
+int maxValue = FindMaxValue(randomNumber);
+Console.WriteLine(maxValue);
